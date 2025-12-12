f9fd8ddb26f5aebbd665bf67e277c5138b776f62
Farizgeloz <farizgeloz26@gmail.com>
2025-12-12 16:48:55 +0700

    resolve: update satuadmin4

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -9,15 +9,15 @@ const api_url_satudata = axios.create({
 
 const api_url_satuadmin = axios.create({
   baseURL: "http://localhost:3010/",
-  /* baseURL: "https://api-satu.mataprabulinggih.net/", */
+  baseURL: "https://api-satu.mataprabulinggih.net/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const api_url_satuadmin_create = axios.create({
-  baseURL: "http://localhost:3010/",
-  /* baseURL: "https://api-satu.mataprabulinggih.net/", */
+  /* baseURL: "http://localhost:3010/", */
+  baseURL: "https://api-satu.mataprabulinggih.net/",
 });
 
 export { api_url_satudata, api_url_satuadmin, api_url_satuadmin_create };
